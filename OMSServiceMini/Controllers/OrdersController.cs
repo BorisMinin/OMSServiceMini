7f97c85789557d5d611b0748b69124888e712e5a
Boris Minin <32355926+BorisMinin@users.noreply.github.com>
2020-07-22 14:50:25 +0300

    Add GET requests at OrdersController Add some NuGet libraries

--- OMSServiceMini/Controllers/OrdersController.cs
+++ OMSServiceMini/Controllers/OrdersController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OMSServiceMini.Data;
+using OMSServiceMini.Models;
+
+//using Microsoft.EntityFrameworkCore;
 
 namespace OMSServiceMini.Controllers
 {
@@ -20,5 +24,32 @@ namespace OMSServiceMini.Controllers
         }
 
         // GET api/orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetAllOrders()
+        {
+            return await _northwindContext.Orders.ToListAsync();
+        }
+
+        // GET api/orders/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrderId(int id)
+        {
+            var order = await _northwindContext.Orders
+                .Where(o => o.OrderId == id)
+                .Include(o => o.OrderDetails)
+                .FirstOrDefaultAsync();
+
+            if (order == null)
+                return NotFound("Заказ с данным id не найден");
+
+            return order;
+        }
+
+        ////POST api/orders/id
+        //[HttpPost ("{id}")]
+        //public async Task<ActionResult<Order>> PostOrder()
+        //{
+
+        //}
     }
 }
