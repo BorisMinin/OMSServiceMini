5784ca9588a908ba15794ae511bb4df8cb1bea1f
Boris Minin <32355926+BorisMinin@users.noreply.github.com>
2020-07-24 13:26:17 +0300

    Edit class Startup Add GET requests

---
+++ OMSServiceMini/Controllers/OrdersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OMSServiceMini.Data;
+using OMSServiceMini.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using NSwag.Generation.Processors;
+
+namespace OMSServiceMini.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        readonly NorthwindContext _northwindContext;
+
+        public OrdersController(NorthwindContext northwindContext)
+        {
+            _northwindContext = northwindContext;
+        }
+
+        // GET: api/orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetAllOrders()
+        {
+            var orders = await _northwindContext.Orders
+                //.Include(o => o.OrderDetails)
+
+                .ToListAsync();
+            return orders;
+        }
+
+        // GET: api/orders/10248
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _northwindContext.Orders
+                .Where(o => o.OrderId == id)
+                .Include(o => o.OrderDetails)
+                .FirstOrDefaultAsync();
+            if (order == null) 
+                return NotFound("Заказ с данным Id не найден");
+
+            return order;
+        }
+    }
+}
--- OMSServiceMini/Startup.cs
+++ OMSServiceMini/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -50,6 +50,10 @@ namespace OMSServiceMini
                     };
                 };
             });
+
+            // https://stackoverflow.com/questions/59199593/net-core-3-0-possible-object-cycle-was-detected-which-is-not-supported
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
